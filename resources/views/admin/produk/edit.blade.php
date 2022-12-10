7466afef4452eb2cd72249cd80ceb4e63be60394
indahqhoirunnisa <indahnishaa@gmail.com>
2022-12-10 16:44:46 +0700

    gambar

--- resources/views/admin/produk/edit.blade.php
+++ resources/views/admin/produk/edit.blade.php
@@ -61,7 +61,7 @@
     </div> 
     <div class="mb-6">
         <label for="gambar" class="block mb-2 text-sm font-medium text-gray-900">Gambar</label>
-        <input type="file" name="gambar[]" id="gambar" class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer" multiple accept="image/*">
+        <input type="file" name="gambar" id="gambar" class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer" multiple accept="image/*">
     </div>
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
