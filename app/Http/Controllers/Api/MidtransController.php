eec5ff21704604edf5b83057323e5628f34d24a3
indahqhoirunnisa <indahnishaa@gmail.com>
2022-12-12 14:31:56 +0700

    DFG

--- app/Http/Controllers/Api/MidtransController.php
+++ app/Http/Controllers/Api/MidtransController.php
@@ -14,9 +14,9 @@ class MidtransController extends Controller
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
         \Midtrans\Config::$isProduction = true;
         // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
+        \Midtrans\Config::$isSanitized = FALSE;
         // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$is3ds = FALSE;
 
         $params = array(
             'transaction_details' => array(
