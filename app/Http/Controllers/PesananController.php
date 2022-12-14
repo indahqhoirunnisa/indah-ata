df187dad0ae5de3c4bed911de9054038f1481845
indahqhoirunnisa <indahnishaa@gmail.com>
2022-12-14 10:17:52 +0700

    se

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -91,7 +91,7 @@ class PesananController extends Controller
         public function detailriwayat($id)
         {
             $Pesanan = Pesanan::where('order_id', $id)->get();
-            return view('pelanggan.detailriwayat', compact('pesanan'));
+            return view('detailriwayat', compact('pesanan'));
         }
     }
 
